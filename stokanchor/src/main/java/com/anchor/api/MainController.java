765372a442e5813cec794a0b5e51dc381487d757
Aubrey Malabie <malengatiger@gmail.com>
2020-04-02 15:20:53 +0200

    🍀 added stellar.toml in application resources to be served as .well-known/stellar.toml

--- stokanchor/src/main/java/com/anchor/api/MainController.java
+++ stokanchor/src/main/java/com/anchor/api/MainController.java
@@ -7,9 +7,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import shadow.org.apache.commons.io.FileUtils;
+import shadow.org.apache.commons.io.IOUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Date;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 @RestController
@@ -23,6 +30,32 @@ public class MainController {
     @Autowired
     private ApplicationContext context;
 
+    @GetMapping(value = "/.well-known/stellar.toml", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public @ResponseBody byte[] getStellarToml() throws Exception {
+        LOGGER.info("\uD83D\uDD35 \uD83D\uDD35 \uD83D\uDD35 get stellar.toml file and return to caller...");
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("_well-known/stellar.toml")).getFile());
+        if (file.exists()) {
+            LOGGER.info(" \uD83C\uDF45 File has been found \uD83C\uDF45 " + file.getAbsolutePath());
+            return IOUtils.toByteArray(new FileInputStream(file));
+        } else {
+            LOGGER.info(" \uD83C\uDF45 File NOT found. this is where .toml needs to go;  \uD83C\uDF45 ");
+            throw new Exception("stellar.toml not found");
+        }
+    }
+    @GetMapping(value = "/.stellar.toml", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public @ResponseBody byte[] getStellarTomlToo() throws Exception {
+        LOGGER.info("\uD83D\uDD35 \uD83D\uDD35 \uD83D\uDD35 get stellar.toml file and return to caller...");
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("_well-known/stellar.toml")).getFile());
+        if (file.exists()) {
+            LOGGER.info(" \uD83C\uDF45 File has been found \uD83C\uDF45 " + file.getAbsolutePath());
+            return IOUtils.toByteArray(new FileInputStream(file));
+        } else {
+            LOGGER.info(" \uD83C\uDF45 File NOT found. this is where .toml needs to go;  \uD83C\uDF45 ");
+            throw new Exception("stellar.toml not found");
+        }
+    }
     @GetMapping("/ping")
     public String ping() {
         LOGGER.info("\uD83D\uDD35 \uD83D\uDD35 \uD83D\uDD35 Pinging StokkieAnchorApplication ...");
@@ -41,7 +74,8 @@ public class MainController {
         Anchor anchor = service.createAnchorAccounts(anchorBag.getAnchor(),
                 anchorBag.getPassword(),anchorBag.getAssetCode(),anchorBag.getAssetAmount());
         LOGGER.info("\uD83E\uDD66 \uD83E\uDD66 \uD83E\uDD66 Stellar returns Anchor: \uD83C\uDF4E "
-                + anchor.getName() + " anchorId: " + anchor.getAnchorId());
+                + anchor.getName() + "  \uD83C\uDF4E anchorId: " + anchor.getAnchorId()
+        + "  \uD83C\uDF4E");
         return anchor;
     }
     @PostMapping("/createUser")
